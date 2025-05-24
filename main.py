eff1e5f2c1fec1a1d4aeb9e5ae9650f34b79a29f
BLOC3AN <lethhai3003@gmail.com>
2025-05-25 00:24:01 +0700

    edit main.py

--- main.py
+++ main.py
@@ -13,18 +13,13 @@ if __name__ == "__main__":
     with open("/home/hai/agent2agent/tools/retrieve.py", "r") as f:
         code_sample = f.read()
     f.close()
-    with open("/home/hai/agent2agent/docs/example_doc.md", "r") as f:
-        pattem_doc = f.read()
-    f.close()
-
-
     # 1. Lấy context
-    ctx = retrieve_knowledge("Bạn là một chuyên gia về viết document dạng mardown với từng suy luận logic để do người dùng dễ dàng đọc tài liệu củas bạn viết")
+    ctx = retrieve_knowledge("Bạn là một chuyên gia về viết document và trình bảy dạng mardown với từng suy luận logic bám sát tài liệu để do người dùng dễ dàng đọc tài liệu của bạn viết")
 
     # 2. Gửi prompt
     prompt = (f"""
-        Phân tích code sau , dùng context {ctx} để cải thiện độ chính xác, 
-        viết tài liệu dạng markdow tham khảo {pattem_doc} để đọc và lưu file:+{code_sample}
+        Phân tích code sau +{code_sample} , dùng context {ctx} để cải thiện độ chính xác, 
+        viết tài liệu dạng markdow theo cú pháp để đọc và lưu file:
     """)
     result = agent.run(prompt)
     print("Kết quả:", result)
